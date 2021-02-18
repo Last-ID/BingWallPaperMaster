460a4290f40296809b97ef8c5a64b0d6670b410d
Last-ID <Last-ID@users.noreply.github.com>
2021-02-18 14:50:26 +0800

    LunaNewYearStyle
    牛年特色

--- assets/py/defaultbg.py
+++ assets/py/defaultbg.py
@@ -1,7 +1,8 @@
 import os
 import shutil
 
-file_dir = "../img"
-os.chdir(file_dir)
-shutil.copyfile("test_org.","test_copy.txt")        # copy test_org.txt 为 test_copy.txt 若存在，则覆盖
-shutil.copyfile("test_org.txt","test1.txt")            # 存在，覆盖
+project_path = "C:/Last_ID-mybugs/BingWallPaperMaster"
+
+img_dir = project_path+"/assets/img"
+os.chdir(img_dir)
+shutil.copyfile("defaultbg.png","bg.jpg")        # copy test_org.txt 为 test_copy.txt 若存在，则覆盖
